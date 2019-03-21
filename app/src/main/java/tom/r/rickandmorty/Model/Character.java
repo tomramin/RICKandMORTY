92947075e92eb1be87a305998e071eb7ff0efb08
Tom <tramin@et.esiea.fr>
2019-03-21 09:42:33 +0100

    add object Character.java(class)

---
+++ app/src/main/java/tom/r/rickandmorty/Model/Character.java
@@ -0,0 +1,59 @@
+package tom.r.rickandmorty.Model;
+
+public class Character {
+
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
